3d4cf8c71276722bcd3a7e31fbf72d4ec971f2b5
celcius-plus-273 <kelvinandreshau@gmail.com>
2024-04-12 10:46:47 -0400

    moved some stuff

--- tests/motor-test.py
+++ tests/motor-test.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         MotorPins(pwm=12, inA=20, inB=21),  # motor 0 pins
         MotorPins(pwm=13, inA=5, inB=6),    # motor 1 pins
         MotorPins(pwm=18, inA=23, inB=24),  # motor 2 pins
-        MotorPins(pwm=19, inA=2 , inB=3)    # motor 3 pins
+        MotorPins(pwm=19, inA=2, inB=3)    # motor 3 pins
     ]
 
     # intialize motor driver
